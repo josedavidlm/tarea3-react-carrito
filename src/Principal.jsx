21b5c83cbc0ad9b23791f2bf210c8c23ec01a234
Jose David Lagos Morales <especialista_nsrtm19@mef.gob.pe>
2023-10-05 23:35:54 -0500

    main

--- src/Principal.jsx
+++ src/Principal.jsx
@@ -1,10 +1,12 @@
 import { CardProducto } from "./components/CardProducto "
 
+
 export const Principal = () =>{
 
     return(
         
         <>          
+        
             <CardProducto/>
         </>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './css/index.css'
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
+import { Producto } from './components/Producto';
 import { Carrito } from './components/carrito';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AppContext>
     <Router>
   <React.StrictMode>
     <Navbar/>
-    <Routes>
+    <Routes>            
           <Route path="/" element={<Principal />} />
-          <Route path="/cart" element={<Carrito />} />
+          <Route path="/cart" element={<Carrito/>} />
     </Routes>
   </React.StrictMode>
   </Router>   
